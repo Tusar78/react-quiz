5f64f4817a7f1cde615659f925b0c040a408c681
Tusar <tusarahmed141@gmail.com>
2022-12-10 18:07:13 +0600

    Created two pages called signup and illustration and one moduler css called illustration.module.css

---
+++ src/Components/Pages/Illustration.js
@@ -0,0 +1,12 @@
+import React from "react";
+import signupImage from '../../assets/images/signup.svg';
+
+const Illustration = () => {
+  return (
+    <div class="illustration">
+      <img src={signupImage} alt="Signup" />
+    </div>
+  );
+};
+
+export default Illustration;
---
+++ src/Components/Pages/Signup.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <>
+      <h1>Create an account</h1>
+      <div className="column">
+        <Illustration />
+        <form action="#" className=''>
+          
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
