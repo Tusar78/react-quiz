f9a25a652a0f61c3e9a830e5b21f223b04330e2b
Tusar <tusarahmed141@gmail.com>
2022-12-10 19:08:18 +0600

    Create one component with a moduler css called TextInput

--- src/Components/Pages/Signup.js
+++ src/Components/Pages/Signup.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "../../styles/Signup.module.css";
 import Form from "../Form";
+import TextInput from "../TextInput";
 import Illustration from "./Illustration";
 
 const Signup = () => {
@@ -10,7 +11,18 @@ const Signup = () => {
       <div className="column">
         <Illustration />
         <Form className={classes.signup}>
-          
+          <TextInput type="text" placeholder="Enter name" icon="person" />
+          <TextInput
+            type="email"
+            placeholder="Enter email"
+            icon="alternate_email"
+          />
+          <TextInput type="password" placeholder="Enter password" icon="lock" />
+          <TextInput
+            type="password"
+            placeholder="Confirmed password"
+            icon="lock_lock"
+          />
         </Form>
       </div>
     </>
