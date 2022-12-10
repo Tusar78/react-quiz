cbdc9e332e90462101cbe853e9aeaf2e61787002
Tusar <tusarahmed141@gmail.com>
2022-12-10 18:57:57 +0600

    Update sign up and form page

--- src/Components/Pages/Signup.js
+++ src/Components/Pages/Signup.js
@@ -9,7 +9,7 @@ const Signup = () => {
       <h1>Create an account</h1>
       <div className="column">
         <Illustration />
-        <Form>
+        <Form className={classes.signup}>
           
         </Form>
       </div>
