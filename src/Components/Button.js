e072d0acc52e579a5c930b4245d3acd8333decac
Tusar <tusarahmed141@gmail.com>
2022-12-11 15:56:03 +0600

    Updated progressBar and button components

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 import classes from "../styles/Button.module.css";
 
-const Button = ({ children }) => {
+const Button = ({ className, children }) => {
   return (
-    <div className={classes.button}>
+    <div className={`${classes.button} ${className}`}>
       <span>{children}</span>
     </div>
   );
