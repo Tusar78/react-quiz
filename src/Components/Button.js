29b11704d74e7600a6f4bba119d06a5fa97fd8dd
Tusar <tusarahmed141@gmail.com>
2022-12-11 16:10:49 +0600

    Update two component miniplayer and progressbar and also minimal update on button.module.css

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -2,11 +2,7 @@ import React from "react";
 import classes from "../styles/Button.module.css";
 
 const Button = ({ className, children }) => {
-  return (
-    <div className={`${classes.button} ${className}`}>
-      <span>{children}</span>
-    </div>
-  );
+  return <div className={`${classes.button} ${className}`}>{children}</div>;
 };
 
 export default Button;
