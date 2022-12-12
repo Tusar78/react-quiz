bd1047449d1fa2e09c3698ca84554f9be0b22eb4
Tusar <tusarahmed141@gmail.com>
2022-12-12 23:35:27 +0600

    Active signup section

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,15 +3,15 @@ import Layout from "./Layout";
 // import Quiz from "./Pages/Quiz";
 // import Result from "./Pages/Result";
 // import Login from "./Pages/Login";
-// import Signup from "./Pages/Signup";
-import Home from "./Pages/Home";
+import Signup from "./Pages/Signup";
+// import Home from "./Pages/Home";
 
 function App() {
   return (
     <>
       <Layout>
-        <Home />
-        {/* <Signup /> */}
+        {/* <Home /> */}
+        <Signup />
         {/* <Login /> */}
         {/* <Quiz /> */}
         {/* <Result /> */}
