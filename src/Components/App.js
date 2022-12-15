fd204fb86fb379b5e1b8888c96a4a924b92d8e63
Tusar <tusarahmed141@gmail.com>
2022-12-15 18:11:34 +0600

    Successfully setup react router

--- src/Components/App.js
+++ src/Components/App.js
@@ -11,16 +11,17 @@ import Signup from "./Pages/Signup";
 function App() {
   return (
     <>
-      <Routes>
-        <Layout>
+      <Layout>
+        <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/quiz" element={<Quiz />} />
           <Route path="/result" element={<Result />} />
           <Route path="*" element={<PageNotFound />} />
-        </Layout>
-      </Routes>
+        </Routes>
+      </Layout>
     </>
   );
 }
