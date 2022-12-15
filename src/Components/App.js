8ca2097a8c71b8f680ea970e517d7e20f9f76bee
Tusar <tusarahmed141@gmail.com>
2022-12-15 18:03:44 +0600

    Routing all components and created one another componet called Page not found

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "../styles/App.css";
 import Layout from "./Layout";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 import Quiz from "./Pages/Quiz";
 import Result from "./Pages/Result";
-import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
-import Home from "./Pages/Home";
 
 function App() {
   return (
     <>
-      <Layout>
-        <Home />
-        <Signup />
-        <Login />
-        <Quiz />
-        <Result />
-      </Layout>
+      <Routes>
+        <Layout>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Layout>
+      </Routes>
     </>
   );
 }
