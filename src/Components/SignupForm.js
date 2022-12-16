5b5fed77c6c14fbb95cae6cbaa3faa732260d009
Tusar <tusarahmed141@gmail.com>
2022-12-16 19:24:51 +0600

    Error message succesfully showd

--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -38,44 +38,48 @@ const SignupForm = () => {
   };
 
   return (
-    <Form style={{ height: "500px" }}>
+    <Form style={{ height: "500px" }} onSubmit={handleForm}>
       <TextInput
         type="text"
         placeholder="Enter name"
         icon="person"
         value={username}
-        onchange={(e) => setUsername(e.target.value)}
+        onChange={(e) => setUsername(e.target.value)}
       />
       <TextInput
         type="email"
         placeholder="Enter email"
         icon="alternate_email"
         value={email}
-        onchange={(e) => setEmail(e.target.value)}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <TextInput
         type="password"
         placeholder="Enter password"
         icon="lock"
         value={password}
-        onchange={(e) => setPassword(e.target.value)}
+        onChange={(e) => setPassword(e.target.value)}
       />
       <TextInput
         type="password"
         placeholder="Confirmed password"
         icon="lock_clock"
         value={confirmedPassword}
-        onchange={(e) => setConfirmedPassword(e.target.value)}
+        onChange={(e) => setConfirmedPassword(e.target.value)}
       />
       <Checkbox
         text="I agree to the Terms &amp; Conditions"
         value={agree}
-        onchange={(e) => setAgree(e.target.value)}
+        onChange={(e) => setAgree(e.target.value)}
       />
-      <Button>Submit now</Button>
+      <Button disabled={loading} type="button">
+        Submit now
+      </Button>
       <div className="info">
         Already have an account? <Link to="/login">Login</Link> instead.
       </div>
+
+      {error && <p className="error">{error}</p>}
     </Form>
   );
 };
