9903bbdcd815d4d7ad5651863c2d0c5b568ee859
Tusar <tusarahmed141@gmail.com>
2022-12-16 17:29:01 +0600

    Also added signupform at signup component

--- src/Components/Pages/Signup.js
+++ src/Components/Pages/Signup.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import signupImage from "../../assets/images/signup.svg";
-import classes from "../../styles/Signup.module.css";
-import Button from "../Button";
-import Checkbox from "../Checkbox";
-import Form from "../Form";
 import Illustration from "../Illustration";
-import TextInput from "../TextInput";
+import SignupForm from "../SignupForm";
 
 const Signup = () => {
   return (
@@ -16,7 +11,7 @@ const Signup = () => {
         <Illustration>
           <img src={signupImage} alt="Signup" />
         </Illustration>
-        
+        <SignupForm />
       </div>
     </>
   );
