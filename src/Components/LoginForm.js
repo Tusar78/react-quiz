b2d757230eba8c226822a29daa509af06c09ccfb
Tusar <tusarahmed141@gmail.com>
2022-12-16 23:33:58 +0600

    Login form component is done

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,9 +1,18 @@
-import React from "react";
-import { Form, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 import Button from "./Button";
+import Form from "./Form";
 import TextInput from "./TextInput";
 
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState("");
+
+  const {login} = useAuth();
+
   return (
     <Form style={{ height: "330px" }}>
       <TextInput type="text" placeholder="Enter email" icon="alternate_email" />
