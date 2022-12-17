9af0aa21cbcc700337c4587b739137ff885a2841
Tusar <tusarahmed141@gmail.com>
2022-12-17 16:03:37 +0600

    Private route complete on using nested routing

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,7 +8,7 @@ import PageNotFound from "./Pages/PageNotFound";
 import Quiz from "./Pages/Quiz";
 import Result from "./Pages/Result";
 import Signup from "./Pages/Signup";
-import PrivetRoute from "./PrivetRoute";
+import PrivateOutlet from "./PrivateOutlet";
 
 function App() {
   return (
@@ -20,22 +20,18 @@ function App() {
             <Route path="/home" element={<Home />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
-            <Route
+            {/* <Route
               path="/quiz"
               element={
                 <PrivetRoute>
                   <Quiz />
                 </PrivetRoute>
               }
-            />
-            <Route
-              path="/result"
-              element={
-                <PrivetRoute>
-                  <Result />
-                </PrivetRoute>
-              }
-            />
+            /> */}
+            <Route path="/*" element={<PrivateOutlet />}>
+              <Route path="quiz" element={<Quiz />} />
+              <Route path="result" element={<Result />} />
+            </Route>
             <Route path="*" element={<PageNotFound />} />
           </Routes>
         </Layout>
