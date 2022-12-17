4bc1ee7123f7f373286697e410df90d56e132316
Tusar <tusarahmed141@gmail.com>
2022-12-17 17:01:16 +0600

    Change location on video component

--- src/Components/Pages/Videos.js
+++ src/Components/Pages/Videos.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import classes from "../../styles/Videos.module.css";
-import Video from "./Video";
+import Video from "../Video";
 
 const Videos = () => {
   return (
