bb200078e045cd7e8d2669fa88e90e684a0a90d1
Tusar <tusarahmed141@gmail.com>
2022-12-18 14:37:49 +0600

    Get database using firebase databases

--- src/hooks/useVideoList.js
+++ src/hooks/useVideoList.js
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
+import { getDatabase } from "firebase/database";
 
 const useVideoList = () => {
   useEffect(() => {
     const fetchVideos = async () => {
-
+      const db = getDatabase();
     };
 
     fetchVideos()
