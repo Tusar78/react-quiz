41ebaf02e8312715fc9465bd00ca6eff6a0c2c8b
Tusar <tusarahmed141@gmail.com>
2022-12-19 19:10:35 +0600

    Declare and initialize two state called loadin gor error

--- src/hooks/useVideoList.js
+++ src/hooks/useVideoList.js
@@ -1,12 +1,20 @@
 import { getDatabase, orderByKey, query, ref } from "firebase/database";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const useVideoList = () => {
+  const [loading,  setLoading] = useState(true);
+  const [error,  setError] = useState(false);
   useEffect(() => {
     const fetchVideos = async () => {
       const db = getDatabase();
       const videoRef = ref(db, "videos");
       const videoQuery = query(videoRef, orderByKey());
+
+      try {
+
+      } catch (err) {
+        console.log(err);        
+      }
     };
 
     fetchVideos();
