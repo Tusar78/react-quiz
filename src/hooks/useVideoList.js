531e16d04bfd6f936119839bec4d396ea5e870c9
Tusar <tusarahmed141@gmail.com>
2022-12-19 19:07:07 +0600

    We are going to be database authentication

--- src/hooks/useVideoList.js
+++ src/hooks/useVideoList.js
@@ -1,13 +1,15 @@
+import { getDatabase, orderByKey, query, ref } from "firebase/database";
 import { useEffect } from "react";
-import { getDatabase } from "firebase/database";
 
 const useVideoList = () => {
   useEffect(() => {
     const fetchVideos = async () => {
       const db = getDatabase();
+      const videoRef = ref(db, "videos");
+      const videoQuery = query(videoRef, orderByKey());
     };
 
-    fetchVideos()
+    fetchVideos();
   }, []);
 };
 
