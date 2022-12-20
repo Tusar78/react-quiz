7b5208f9cb0fe4a1c6dc74ff10c3263bdecbdc31
Tusar <tusarahmed141@gmail.com>
2022-12-20 12:21:36 +0600

    I think get video is notworking

--- src/hooks/useVideoList.js
+++ src/hooks/useVideoList.js
@@ -8,8 +8,8 @@ const useVideoList = () => {
   useEffect(() => {
     const fetchVideos = async () => {
       const db = getDatabase();
-      const videoRef = ref(db, "videos");
-      const videoQuery = query(videoRef, orderByKey());
+      const videosRef = ref(db, "videos");
+      const videoQuery = query(videosRef, orderByKey());
 
       try {
         setError(false);
